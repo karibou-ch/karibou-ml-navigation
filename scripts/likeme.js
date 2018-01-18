beb360c79abc177a1bb9c4d86de6473824a457e7
Daniel Teixeira <ddtxra@gmail.com>
2018-01-18 22:31:47 +0100

    adding first version

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -1,14 +1,65 @@
-var Recommender = require('likely.js');
+var Recommender = require('likely');
+var json2csv = require('json2csv');
+var fs = require('fs');
 
-var rowLabels = ['John', 'Sue', 'Joe'];
-var colLabels = ['Red', 'Blue', 'Green', 'Purple'];
+var orders = require('../test/data/orders2.json');
+  
+//Traversing orders to find the distinct list of customers and products
+var customers = []
+var products = []
+var customerProducts = {}
+orders.forEach(element => {
+	let ps = element.customer.pseudo;
+	if(customers.indexOf(ps) == -1){
+		customers.push(ps)
+	}
+	element.items.forEach(item => {
+		if(products.indexOf(item.category) == -1){
+			products.push(item.category);
+		}
+	})
+});
+
+console.log("customers " , customers.length , " example [", customers[0] , ", " , customers[1] , " ...")
+console.log("products " , products.length , " example [", products[0] , ", " , products[1] , " ...")
+
+//Initializing an empty matrix with n rows for customers and m columns for products found
+var matrix = [];
+for(var i=0; i<customers.length; i++) {
+    matrix[i] = [];
+    for(var j=0; j<products.length; j++) {
+        matrix[i][j] = 0;
+    }
+}
+
+//Building matrix with customer x products with values of orders
+orders.forEach(element => {
+	element.items.forEach(item => {
+		let pseudoid = customers.indexOf(element.customer.pseudo);
+		let productid = products.indexOf(item.category);
+		matrix[pseudoid][productid]++;
+	})
+});
+
+//Save to file
+var logger = fs.createWriteStream('matrix.csv');
+logger.write("customer," + products.join(",") + "\n")
+for(var i=0; i<customers.length; i++) {
+	logger.write(customers[i] + "," + matrix[i].join(",") + "\n")
+}
+logger.end()
+
+
+console.log("Building model")
 
 // Build the model using the training set
-var model = Recommender.buildModel(trainingSet, rowLabels, colLabels);
+var model = Recommender.buildModel(matrix, customers, products);
 
-// Calculate the error from the produced model against the CV set of known values
-var totalError = Recommender.calculateTotalError(model.estimate, crossValidationSet);
+console.log("Model built (DONE)")
 
-//
-var recommendations = model.recommendations('John');
-var allItems = model.rankAllItems('John');
+console.log("Recommending ...")
+
+var recommendations = model.recommendations('C**D');
+console.log("Recommendation DONE")
+
+console.log(recommendations)
